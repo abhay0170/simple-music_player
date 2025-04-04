ca84474ad5756299fadf56c1056fae58b9cac9a1
abhay0170 <abhay01070@gmail.com>
2025-04-04 15:08:10 +0530

    added themeprovider

---
+++ lib/theme/theme_provider.dart
@@ -0,0 +1,21 @@
+  import 'package:flutter/material.dart';
+import 'package:simple_music_player/theme/dark_theme.dart';
+import 'package:simple_music_player/theme/light_theme.dart';
+
+class ThemeProvider extends ChangeNotifier {
+
+    // initially ,light mode 
+    ThemeData _themeData=lightMode;
+
+    // get theme
+    ThemeData themeData=> _themeData;
+
+    // change to dark mode
+    bool get  isDarkMode => _themeData==darkMode;
+
+    // set theme
+    set themeData(ThemeData themeData){
+      _themeData=themeData;
+      notifyListeners();
+    }
+  }
